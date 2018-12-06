4f9394384c01452626fa93e0f130ceaa8be3b147
mocio <vivaldi@arosedesign.com>
2018-12-07 00:25:03 +0100

    composer per form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+
 
 class HomeController extends Controller
 {
@@ -23,6 +25,24 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $users = User::all();
+
+        return view('home')->with('users',$users);
+    }
+
+    public function editUser($params)
+    {
+        $users = App\User::findOrFail($params['id']);
+
+        if($params['azione'] == 'elimina') {
+            $users->delete();
+        } elseif ($params['azione'] == 'rendiadmin') {
+            $users->role = 'admin';
+            $users->save();
+        }
+
         return view('home');
     }
+
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    @if(Auth::user()->role == 'admin')
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Gestione utenti</div>
 
-                    You are logged in!
+                    <div class="panel-body">
+                        <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>
+                                    {{ Form::open(['method' => 'DELETE', 'route' => ['comics.destroy', $user->id]]) }}
+                                    {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                    {{ Form::close() }}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @else
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">HAI CORRETTAMENTE EFFETTUATO LA LOGIN</div>
+
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        L'amministratore deve ancora accettare la tua registrazione.<br>Per favore, riprova più tardi.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+     @endif
 </div>
 @endsection
